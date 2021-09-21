3b1804072e19c38e37047e76574f32c2edbfcb11
Terry Mace <terry.mace@payroc.com>
2021-09-21 11:43:25 +0100

    Defined a Test Header middleware class using Custom Middleware conventions

---
+++ WeatherApi/Middleware/TestHeaderMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WeatherApi.Middleware
+{
+    public class TestHeader
+    {
+        private readonly RequestDelegate next;
+
+        public TestHeader(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // do something
+            await next(httpContext);
+        }
+    }
+}
